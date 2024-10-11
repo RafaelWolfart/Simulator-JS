1e20ea61eaa11d3f70c5c25a97b186d7d6801344
Rafael Wolfart <rafaelwolfart1@outlook.com>
2024-10-11 00:02:19 -0300

    Agregando boton de eliminar en carrito

--- JS/carrito.js
+++ JS/carrito.js
@@ -8,10 +8,25 @@ function mostrarCarrito(itemCarrito) {
         const cardCompra = document.createElement("div")
         cardCompra.innerHTML = `<h3>${producto.nombre}</h3>
                                 <p>$${producto.precio}</p>
-                                <button class="eliminar-item">Eliminar</button>`
+                                <button class="eliminar-item" id="${producto.id}">Eliminar</button>`
         carritoCompra.appendChild(cardCompra)
     })
 }
 
 mostrarCarrito(carritoCargado)
+
+// eliminar item del carrito
+function eliminarItem() {
+    const botonesEliminar = document.querySelectorAll(".eliminar-item")
+    botonesEliminar.forEach(boton => {
+        boton.onclick = (e) => {
+            const productoId = e.currentTarget.id
+            const index = carritoCargado.findIndex(producto => producto.id == productoId)
+            carritoCargado.splice(index, 1)
+            localStorage.setItem("carrito", JSON.stringify(carritoCargado))
+            location.reload()
+            console.log(carritoCargado)
+        }
+    })
+}
 
